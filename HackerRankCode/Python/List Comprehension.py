8dab112b67b2cda7958a323b289d9615d4f549b6
anushome <91409192+anushome@users.noreply.github.com>
2021-09-26 13:43:01 +0530

    Update List Comprehension.py

--- HackerRankCode/Python/List Comprehension.py
+++ HackerRankCode/Python/List Comprehension.py
@@ -1,4 +1,6 @@
-# Let's learn about list comprehensions! You are given three integers  and  representing the dimensions of a cuboid along with an integer n . Print a list of all possible coordinates given by (i, j, k) on a 3D grid where the sum of i+j+k is not equal to n. Here, 0 <= i <= X; 0 <= j <= Y; 0 <= k <= Z. Please use list comprehensions rather than       multiple loops, as a learning exercise.
+# Let's learn about list comprehensions! You are given three integers  and  representing the dimensions of a cuboid along with an integer n . Print a list of all possible 
+# coordinates given by (i, j, k) on a 3D grid where the sum of i+j+k is not equal to n. Here, 0 <= i <= X; 0 <= j <= Y; 0 <= k <= Z. Please use list comprehensions 
+# rather than multiple loops, as a learning exercise.
 
 
 # Code 
