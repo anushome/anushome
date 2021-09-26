bb0b9d1f63e4c0db6f8ad41711155ff1c53eb2ea
anushome <91409192+anushome@users.noreply.github.com>
2021-09-26 13:43:26 +0530

    Update List Comprehension.py

--- HackerRankCode/Python/List Comprehension.py
+++ HackerRankCode/Python/List Comprehension.py
@@ -10,6 +10,5 @@ if __name__ == '__main__':
     z = int(raw_input())
     n = int(raw_input())
     
-    ans = [[i, j, k] for i in range(x + 1) for j in range(y + 1) for k in range(z + 1)
-    if i+j+k != n       ]
+    ans = [[i, j, k] for i in range(x + 1) for j in range(y + 1) for k in range(z + 1) if i+j+k != n]
     print(ans)
